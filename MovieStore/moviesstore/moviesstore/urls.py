a31e3f115b85a4d7976bae8b4d1f0c1447e2cd66
nm121212 <127545866+nm121212@users.noreply.github.com>
2025-02-03 22:59:00 -0500

    test

--- MovieStore/moviesstore/moviesstore/urls.py
+++ MovieStore/moviesstore/moviesstore/urls.py
@@ -19,5 +19,9 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', include('home.urls')),
+    path('movies/', include('movies.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
+
--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home.index'),
-    path('about/', views.about, name='home.about'),
+    path('admin/', admin.site.urls),
+    path('', include('home.urls')),
+    path('movies/', include('movies.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
