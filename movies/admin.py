10bb7f19fa21598f2d2decf9fff076a1dff24fc6
amoghkumar <amoghk333@gmail.com>
2025-02-03 23:46:23 -0500

    test

--- movies/admin.py
+++ movies/admin.py
@@ -2,3 +2,12 @@ from django.contrib import admin
 from .models import Movie
 admin.site.register(Movie)
 # Register your models here.
+
+from django.contrib import admin
+from home.models import *
+
+# Register your models here.
+admin.site.register(Movie)
+admin.site.register(Review)
+admin.site.register(Order)
+admin.site.register(Customer)
