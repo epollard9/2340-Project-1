546c31caa8cf92569fd22f6c31126438a54187d5
amoghkumar <amoghk333@gmail.com>
2025-02-04 01:34:12 -0500

    test

--- movies/admin.py
+++ movies/admin.py
@@ -4,9 +4,11 @@ from .models import Movie
 
 from django.contrib import admin
 from .models import *
-
+class MovieAdmin(admin.ModelAdmin):
+    ordering = ['name']
+    search_fields = ['name']
 # Register your models here.
-admin.site.register(Movie)
+admin.site.register(Movie, MovieAdmin)
 admin.site.register(Review)
 admin.site.register(Order)
 admin.site.register(Customer)
