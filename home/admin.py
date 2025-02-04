844b4260c78c142d74d4582a61dd72edaca20376
amoghkumar <amoghk333@gmail.com>
2025-02-03 23:21:19 -0500

    test

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from home.models import *
 
 # Register your models here.
+admin.site.register(Movie)
+admin.site.register(Review)
+admin.site.register(Order)
+admin.site.register(Customer)
