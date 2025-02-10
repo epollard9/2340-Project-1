00fa89983761c2c76c7874d0ec9a82d139cfdb4a
EverettP9 <epollard9@gatech.edu>
2025-02-10 15:54:22 -0500

    Possibly fixed rest of merge conflicts

--- movies/admin.py
+++ movies/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Movie, Review, Order, Customer
+from .models import Movie, Review
 class MovieAdmin(admin.ModelAdmin):
     ordering = ['name']
     search_fields = ['name']
@@ -7,5 +7,3 @@ class MovieAdmin(admin.ModelAdmin):
 admin.site.register(Movie, MovieAdmin)
 admin.site.register(Review)
 
-admin.site.register(Order)
-admin.site.register(Customer)
