45eebde36a784ed80bfeddcf2556b4a544b65edd
EverettP9 <epollard9@gatech.edu>
2025-02-10 16:15:48 -0500

    Possibly fixed rest of merge conflicts

--- movies/models.py
+++ movies/models.py
@@ -13,7 +13,7 @@ class Movie(models.Model):
 
 class Review(models.Model):
     id = models.AutoField(primary_key=True)
-    comment = models.CharField(max_length=255)
+    comment = models.CharField(max_length=255, default="")
     date = models.DateTimeField(auto_now_add=True)
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
